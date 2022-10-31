140ffee54f6e50af37fa7626b1308927a29c6b94
Jonathan Y. Fang <f004r94@plank.thayer.dartmouth.edu>
2022-10-31 06:14:36 -0400

    added functionality in word.c for parsing, tokenizing, and validating query strings

--- common/word.c
+++ common/word.c
@@ -11,6 +11,7 @@
 #include <string.h>
 #include "word.h"
 
+
 // changes memory directly, so no need to return a char*
 char *NormalizeWord(char* word) {
     int len = strlen(word);
@@ -22,4 +23,63 @@ char *NormalizeWord(char* word) {
         word[i] = tolower(word[i]);
     }
     return word;
+}
+
+// Helper function to check if word is an operator "and" or "or"
+bool isOperator(char* word) {
+    // strcmp the input to "and" and "or"
+    if (strcmp(word, "and") != 0 && strcmp(word, "or") != 0) {
+        return false;
+    }
+    return true;
+}
+
+// Parses through and makes sure the string is only letters and spaces
+// If so, tokenize the query string (char* line)
+bool tokenizeString(char* line, char* tokenArr[], int* len) {
+    char* word = strtok(line, " ");
+    // Empty query 
+    if (word == NULL) {
+        return false;
+    }
+    int index = 0;
+    // check to make sure the word is alphabetical
+    while(word != NULL) {
+        for(int i = 0; i < strlen(word); i++) {
+            if(!isalpha(word[i])) { 
+                fprintf(stderr, "Error: bad character '%c' in query\n", word[i]);
+                return false;
+            }
+        }
+        // word is alphabetical
+        tokenArr[index] = NormalizeWord(word);
+        word = strtok (NULL, " "); // move to next token
+        index++;
+    }
+    *len = index;
+    return true;
+}
+
+// Validates the query:
+// Neither the first or last word may be an operator, and two operators may not be adjacent. 
+// If valid, return true; otherwise print a suitable error message and return false.
+bool validateQuery(char* tokenArr[], int len) {
+    // Already checked for empty queries
+    // Check for first and last word
+    if (isOperator(tokenArr[0])) {
+        fprintf(stderr, "Error: '%s' cannot be first\n", tokenArr[0]);
+        return false;
+    }
+    else if (isOperator(tokenArr[len-1])) {
+        fprintf(stderr, "Error: '%s' cannot be first\n", tokenArr[len-1]);
+        return false;
+    }
+    // Check for adjacent operators
+    for (int i = 0; i < len - 1; i++) {
+        if (isOperator(tokenArr[i]) && isOperator(tokenArr[i+1])) {
+            fprintf(stderr, "Error: '%s' and '%s' cannot be adjacent\n", tokenArr[i], tokenArr[i+1]);
+            return false;
+        }
+    }
+    return true;
 }
--- common/word.h
+++ common/word.h
@@ -16,4 +16,17 @@
 // Sends all the chars in a str to lowercase
 char* NormalizeWord(char* word);
 
+// Helper function to check if word is an operator "and" or "or"
+bool isOperator(char* word);
+
+// Parses through and makes sure the string is only letters and spaces
+// If so, tokenize the query string (char* line)
+bool tokenizeString(char* line, char* tokenArr[], int* len);
+
+// Validates the query:
+// Neither the first or last word may be an operator, and two operators may not be adjacent. 
+// If valid, return true; otherwise print a suitable error message and return false.
+bool validateQuery(char* tokenArr[], int len);
+
+
 #endif
